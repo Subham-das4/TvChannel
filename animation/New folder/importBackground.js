969230f4d7b538f94ad695d155a7b3e1b8fcb84c
Subham-das4 <100119360+Subham-das4@users.noreply.github.com>
2023-09-23 21:13:14 +0530

    Add files via upload

---
+++ animation/New folder/importBackground.js
@@ -0,0 +1,7 @@
+import background1 from '../assets/background_dubai-min.jpg'
+import background2 from '../assets/background_dubai1-min.jpg'
+import background3 from '../assets/background_dubai3-min.jpg'
+import background4 from '../assets/background_dubai4-min.jpg'
+import background5 from '../assets/background_dubai5.jpg'
+
+export {background1,background2,background3,background4 ,background5}
